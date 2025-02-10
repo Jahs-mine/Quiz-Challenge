6c6245ed9b419d9c9116f37e1a1df6ef2ee2d3b3
Jikiemi Temiloluwa <temiloluwajik@gmail.com>
2025-02-10 20:16:49 +0100

    Add files via upload

---
+++ zeal.js
@@ -0,0 +1,31 @@
+//fill up the lessons
+
+zeal = {
+  "teen": {
+    1: {
+      topic: "",
+      memory_verse_ref: "",
+      memory_verse_text: "",
+      bible_text_ref: "",
+      bible_text_text: "",
+    },
+  },
+  "super-teen": {
+    1: {
+      topic: "",
+      memory_verse_ref: "",
+      memory_verse_text: "",
+      bible_text_ref: "",
+      bible_text_text: "",
+    },
+    24: {
+      topic: "forgiveness",
+      memory_verse_ref: "Matthew 6:14",
+      memory_verse_text:
+        "For if ye forgive men their tresspasses, your heavenly father will also forgive you",
+      bible_text_ref: "Matthew 18:21-22",
+      bible_text_text:
+        "Then came Peter to him, and said,Lord, how oft shall my brother sin against me, and I forgive him? till seven times? Jesus saith unto him, I say not unto thee,Until seven times,but , Until seventy times seven",
+    },
+  },
+};
